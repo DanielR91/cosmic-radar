d32c926d7cd086dbbcf2e8bf9102b1a4203c083c
Daniel <dan.russell2@gmail.com>
2026-06-06 22:29:28 +0100

    Swap CelesTrak target group to stations

--- api/satellites.js
+++ api/satellites.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch('https://celestrak.org/NORAD/elements/gp.php?GROUP=ACTIVE&FORMAT=JSON-PRETTY');
+    const response = await fetch('https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=json-pretty');
     if (!response.ok) {
       throw new Error(`CelesTrak error code: ${response.status}`);
     }
